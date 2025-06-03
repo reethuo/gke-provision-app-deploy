2db25604083bb8e450262f6461c26838773357b6
reethuo <152209084+reethuo@users.noreply.github.com>
2025-06-03 15:50:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,33 +3,16 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-c"
 }
-
 resource "google_container_cluster" "cluster_1" {
   name     = "cluster-1"
   location = "us-west1-c"
-
   release_channel {
     channel = "REGULAR"
   }
-
-  remove_default_node_pool = true
-}
-
-resource "google_container_node_pool" "default_pool" {
-  name       = "default-pool"
-  cluster    = google_container_cluster.cluster_1.name
-  location   = "us-west1-c"
-  node_count = 2
-
-  version = "1.32.4-gke.1106006"
-
+  initial_node_count = 2
   node_config {
     machine_type = "e2-standard-2"
     image_type   = "COS_CONTAINERD"
     disk_size_gb = 20
   }
 }
-
-
-
-
