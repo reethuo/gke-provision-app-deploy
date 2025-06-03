08fecbceb249cf8e69369e9f1782f2e660fceeb7
reethuo <152209084+reethuo@users.noreply.github.com>
2025-06-03 15:43:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,120 +4,32 @@ provider "google" {
   zone    = "us-west1-c"
 }
 
-resource "google_compute_instance_template" "default" {
-  name           = "template-mig"
-  machine_type   = "e2-micro"
-  region         = "us-west1"
+resource "google_container_cluster" "cluster_1" {
+  name     = "cluster-1"
+  location = "us-west1-c"
 
-  tags           = ["mig-instance"]
-
-  disk {
-    auto_delete  = true
-    boot         = true
-    source_image = "projects/centos-cloud/global/images/family/centos-stream-9"
+  release_channel {
+    channel = "REGULAR"
   }
 
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+  remove_default_node_pool = true
+}
 
+resource "google_container_node_pool" "default_pool" {
+  name       = "default-pool"
+  cluster    = google_container_cluster.cluster_1.name
+  location   = "us-west1-c"
+  node_count = 2
 
+  version = "1.32.4-gke.1106006"
 
-  metadata = {
-    ssh-keys = "ansible:${var.public_key}"
-    startup-script       = <<-EOF
-      #!/bin/bash
-      set -e
-
-  # Install required tools
-      dnf install -y yum-utils device-mapper-persistent-data lvm2
-
-  # Add Docker repository
-      dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-  # Install Docker
-      dnf install -y docker-ce docker-ce-cli containerd.io
-
-  # Enable and start Docker
-      systemctl enable docker
-      systemctl start docker
-
-  # Wait for Docker daemon
-      until docker info >/dev/null 2>&1; do
-        echo "Waiting for Docker to start..."
-        sleep 3
-      done
-
-  # Run Harness Delegate
-      docker run --cpus=1 --memory=2g \
-        -e DELEGATE_NAME=reethu-docker \
-        -e NEXT_GEN="true" \
-        -e DELEGATE_TYPE="DOCKER" \
-        -e ACCOUNT_ID=ucHySz2jQKKWQweZdXyCog \
-        -e DELEGATE_TOKEN=NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY= \
-        -e DELEGATE_TAGS="" \
-        -e MANAGER_HOST_AND_PORT=https://app.harness.io \
-        --restart always \
-        --name harness-delegate \
-        -d us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85903
-  EOF
+  node_config {
+    machine_type = "e2-standard-2"
+    image_type   = "COS_CONTAINERD"
+    disk_size_gb = 20
   }
 }
 
-resource "google_compute_region_instance_group_manager" "mig" {
-  name               = "mig-harness"
-  base_instance_name = "mig-instance"
-  region             = "us-west1"
-  version {
-    instance_template = google_compute_instance_template.default.self_link
-  }
-
-  target_size = 2
-
-  auto_healing_policies {
-    health_check      = google_compute_health_check.default.self_link
-    initial_delay_sec = 60
-  }
-}
-
-resource "google_compute_health_check" "default" {
-  name               = "example-health-check"
-  check_interval_sec = 10
-  timeout_sec        = 5
-  healthy_threshold  = 2
-  unhealthy_threshold = 2
-
-  tcp_health_check {
-    port = 80
-  }
-}
-
-resource "google_compute_region_autoscaler" "default" {
-  name   = "example-autoscaler"
-  region = "us-west1"
-  target = google_compute_region_instance_group_manager.mig.self_link
-
-  autoscaling_policy {
-    max_replicas    = 5
-    min_replicas    = 2
-    cooldown_period = 60
-
-    cpu_utilization {
-      target = 0.6
-    }
-  }
-}
-
-
-variable "public_key" {
-  type = string
-  default = ""
-}
-
-output "template_metadata" {
-  value = google_compute_instance_template.default.metadata
-}
 
 
 
