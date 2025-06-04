110b47c2eb11c1820e611be1f65dad8ea1fd4e4f
reethuo <152209084+reethuo@users.noreply.github.com>
2025-06-04 13:51:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,14 +31,7 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = google_container_cluster.cluster_1.endpoint
-    cluster_ca_certificate = base64decode(google_container_cluster.cluster_1.master_auth[0].cluster_ca_certificate)
-    token                  = data.google_client_config.default.access_token
-    load_config_file       = false
-  }
-}
+
 
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
@@ -74,4 +67,29 @@ resource "helm_release" "hello_world" {
   ]
 
   depends_on = [google_container_cluster.cluster_1]
+}
+
+module "delegate" {
+  source = "harness/harness-delegate/kubernetes"
+  version = "0.2.2"
+
+  account_id = "ucHySz2jQKKWQweZdXyCog"
+  delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
+  delegate_name = "terraform-delegate-reethu"
+  deploy_mode = "KUBERNETES"
+  namespace = "harness-delegate-ng"
+  manager_endpoint = "https://app.harness.io"
+  delegate_image = "us-docker.pkg.dev/gar-prod-setup/harness-public/harness/delegate:25.05.85903"
+  replicas = 1
+  upgrader_enabled = true
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.cluster_1.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.cluster_1.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+    load_config_file       = false
+    config_path = "~/.kube/config"
+  }
 }
