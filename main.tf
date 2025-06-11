0463b2e19981dba6de9b9d07d5802465b7f00658
reethuo <152209084+reethuo@users.noreply.github.com>
2025-06-11 16:53:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
+      source  = "hashicorp/null" 
+      version = "~> 3.2" #ensures that Terraform uses version ~> 3.2
     }
   }
 }
@@ -11,22 +11,26 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-c"
 }
-data "google_client_config" "default" {}
+
+data "google_client_config" "default" {} #✅ Purpose
+#This is a Terraform data source from the google provider that fetches information about the currently authenticated Google Cloud client used by Terraform. It's especially useful when you need details like:
+#Project,Region/Zone,Access token and Account being used
+#This is useful in dynamic environments where you want Terraform to adapt to the current GCP configuration((for example, based on gcloud auth login).
 
 resource "google_container_cluster" "cluster_1" {
   name     = "cluster-1"
   location = "us-west1-c"
 
-  release_channel {
-    channel = "REGULAR"
-  }
+  release_channel {      #🎯 Purpose
+    channel = "REGULAR"  #release_channel block tells GKE how often your cluster should get Kubernetes and node updates.
+  }                      #Choosing REGULAR helps you stay reasonably up-to-date without risking the instability of very new versions.
 
   initial_node_count = 2
 
   node_config {
     machine_type = "e2-standard-2"
-    image_type   = "COS_CONTAINERD"
-    disk_size_gb = 20
+    image_type   = "COS_CONTAINERD" #COS stands for Container-Optimized OS, a lightweight, secure OS maintained by Google, specifically designed for running containers.
+    disk_size_gb = 20 #CONTAINERD indicates that the node will use containerd as the container runtime (instead of the older docker runtime).
   }
 deletion_protection=false
 }
